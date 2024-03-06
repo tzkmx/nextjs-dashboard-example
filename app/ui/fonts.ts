daa065538b08636fc4d527a59cbba258910912c1
Jesus Franco Martinez <jesus.franco@ventionteams.com>
2024-03-06 13:30:11 -0600

    Font configuration

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter'
+})
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    variable: '--font-read',
+    weight: '700'
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        lusitana: ['var(--font-read)']
+      },
       gridTemplateColumns: {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
