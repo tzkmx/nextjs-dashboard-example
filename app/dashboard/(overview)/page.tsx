2cd252401bba92adc68ab1b34cae1c391afc19c2
Jesus Franco Martinez <jesus.franco@ventionteams.com>
2024-03-11 09:47:52 -0600

    Implementing Search and Pages

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,7 +9,7 @@ import {
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons'
 
-export default async function Dashboard() {
+export default function Dashboard() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
