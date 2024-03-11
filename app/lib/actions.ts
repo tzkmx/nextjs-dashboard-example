965d8e7b00cd08c10c3600bca18417043f7995f1
Jesus Franco Martinez <jesus.franco@ventionteams.com>
2024-03-11 15:02:21 -0600

    UI for error boundaries

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,11 +24,17 @@ export async function createInvoice(formData: FormData) {
   })
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
-  await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `
-
+  } catch (e) {
+    console.error(e)
+    return {
+      message: 'Error creating invoice',
+    }
+  }
   redirectToDashboardInvoices()
 }
 
@@ -39,23 +45,37 @@ export async function updateInvoice(id: string, formData: FormData) {
     status: formData.get('status'),
   })
   const amountInCents = amount * 100
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId},
-        amount      = ${amountInCents},
-        status      = ${status}
-    WHERE id = ${id}
-  `
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId},
+          amount      = ${amountInCents},
+          status      = ${status}
+      WHERE id = ${id}
+    `
+  } catch (e) {
+    console.error(e)
+    return { message: 'Error updating invoice' }
+  }
   redirectToDashboardInvoices()
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`
-    DELETE
-    FROM invoices
-    WHERE id = ${id}
-  `
+  throw new Error('Failed to Delete Invoice')
+  try {
+    await sql`DELETE
+              FROM invoices
+              WHERE id = ${id}`
+  } catch (e) {
+    console.error(e)
+    return {
+      message: 'Error deleting invoice',
+    }
+  }
   revalidateDashboardInvoices()
+  return {
+    message: 'Invoice deleted',
+  }
 }
 
 function redirectToDashboardInvoices() {
